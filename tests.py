169e5d6ae3086bdd3d06f6736aa6d2971047ec93
Michael Slugocki <SlugocM@users.noreply.github.com>
2017-10-01 19:12:09 -0400

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 29 21:21:23 2017
+
+@author: MetalHorse
+"""
+
+
+# Import data as data frame into working environment using pandas
+data = pd.read_csv('data.csv')
+
+# Initialize options variable as dictionary type
+options = dict()
+
+# Determines the value of gamma (guess rate) to which the function is fit (default = 2)
+# Note: 
+options['nAFC'] = 0
+# Determines the type of sigmoidal curve that is fit to the data (default = 'cnorm')
+options['sigmoidType'] = 'weibull'
+
+model = bayesfit_build(data, options)
+
+fit = bayesfit(data,options,model)
+
+
+params,threshold = bayesfit_extract(fit,options)
+
+
+options['plot'] = 'cdf'
+bayesfit_plot(data,fit,params,options)      
