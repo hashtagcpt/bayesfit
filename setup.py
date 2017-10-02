3a10931438672349683ea316f4fccd4e7ae3d6d2
Michael Slugocki <Mike@vislab-12.psychology.mcmaster.ca>
2017-10-02 11:55:08 -0400

    Add new setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Created on Thu Sep 28 08:56:53 2017
 from setuptools import setup
 
 setup(name='bayesfit',
-      version='0.11',
+      version='1.0',
       description='Bayesian Psychometric Curve Fitting Module',
       url='http://github.com/slugocm/BayesFit',
       author='Michael Slugocki',
